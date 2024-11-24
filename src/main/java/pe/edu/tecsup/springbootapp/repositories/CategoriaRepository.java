029f18576be0ac9478d044d9d75fa4de832407ba
jgomezz <jgomezz@gmail.com>
2024-11-24 11:52:04 -0500

    update JPA in categoria

--- src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepository.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepository.java
@@ -1,12 +1,13 @@
 package pe.edu.tecsup.springbootapp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import pe.edu.tecsup.springbootapp.entities.Categoria;
 
 import java.util.List;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface CategoriaRepository extends CrudRepository<Categoria, Integer> {
 
-    //List<Categoria> findAll() throws Exception;
+    List<Categoria> findAll();
 
 }
